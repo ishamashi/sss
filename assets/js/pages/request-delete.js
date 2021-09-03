4eb4b55ab2b1769b444c490c48d45d7475c86ccd
antoni <antoniyus.silaban@prisma-ads.com>
2021-09-03 14:08:04 +0700

    update form req del

--- assets/js/pages/request-delete.js
+++ assets/js/pages/request-delete.js
@@ -91,13 +91,13 @@ function getArchieveDelete() {
 					"8": v['owner']==highlight?'<font color="red">'+v['archive_date']+'</font>':v['archive_date'],
 					"9": v['owner']==highlight?'<font color="red">'+v['detail_status']['status']+'</font>':v['detail_status']['status'],
 					// "9": '<button class="btn btn-success btn-rounded" title="Rollback" onclick="rollback(\'' + v[0] + '\', \'true\')"><span class="fa fa-history" ></span></button>'
-					"10": '<button class="btn btn-success btn-rounded" data-toggle="modal" data-target=".history_archieve" title="History" onclick="history(\'' + v['ooh_id'] + '\', \'true\', \'' + v['detail_status']['tgl_pengajuan'] + '\')"><span class="fa fa-search" ></span></button>',
-					"11": v['rollback_interval']<=3600?'<button class="btn btn-success btn-rounded" title="Rollback" onclick="rollback(\'' + v['ooh_id'] + '\', \'true\', \'' + v['detail_status']['tgl_pengajuan'] + '\')"><span class="fa fa-history" ></span></button>':'-',
+					// "10": '<button class="btn btn-success btn-rounded" data-toggle="modal" data-target=".history_archieve" title="History" onclick="history(\'' + v['ooh_id'] + '\', \'true\', \'' + v['detail_status']['tgl_pengajuan'] + '\')"><span class="fa fa-search" ></span></button>',
+					"10": v['rollback_interval']<=3600?'<button class="btn btn-success btn-rounded" title="Rollback" onclick="rollback(\'' + v['ooh_id'] + '\', \'true\', \'' + v['detail_status']['tgl_pengajuan'] + '\')"><span class="fa fa-history" ></span></button>':'-',
 				};
 				dattab.push(perdata);
 				no++;
 			});
-			var colome = [{ data: "1"}, { data: "2"}, { data: "3" }, { data: "4" }, { data: "5" }, { data: "6" }, { data: "7" },  { data: "8" }, { data: "9" }, { data: "10" }, {data: "11"}]
+			var colome = [{ data: "1"}, { data: "2"}, { data: "3" }, { data: "4" }, { data: "5" }, { data: "6" }, { data: "7" },  { data: "8" }, { data: "9" }, { data: "10" }]
 			setTableContent('#archieve_del', colome, dattab);
 
 		},
